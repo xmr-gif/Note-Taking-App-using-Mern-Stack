acc204ae315e608fd8e18dea1109357255c12415
OUSSAMA <hafidoss2004@gmail.com>
2025-06-19 10:14:06 +0100

    prepared for the deployment

--- backend/src/server.js
+++ backend/src/server.js
@@ -1,6 +1,7 @@
 import express from "express";
 import cors from "cors";
 import dotenv from "dotenv" ;
+import path from "path"
 
 import notesRoutes from "./routes/notesRoutes.js";
 import { connectDB } from "./config/db.js";
@@ -10,16 +11,29 @@ import rateLimiter from "./middleware/rateLimiter.js";
 dotenv.config() ;
 const app = express();
 const PORT = process.env.PORT || 5001 ;
+const __dirname = path.resolve()
+
+
+//app.use(cors()) ;  allows every request from every single url
+// to specify :
+if(process.env.NODE_ENV !== "production"){
+    app.use(cors({
+        origin:"http://localhost:5173" ,
+    }));
+}
 
 //middleware
 app.use(express.json()); // this middleware will parse JSON bodies : req.body
-//app.use(cors()) ;  allows every request from every single url
-// to specify :
-app.use(cors({
-    origin:"http://localhost:5173" ,
-}))
 app.use(rateLimiter) ;
 app.use("/api/notes", notesRoutes) ;
+
+if(process.env.NODE_ENV === "production"){
+    app.use(express.static(path.join(__dirname,"../frontend/dist")));
+
+    app.get("*",(req,res) =>{
+        res.sendFile(path.join(__dirname,"../frontend","dist","index.html"));
+    })
+}
 
 connectDB().then(()=>{
     app.listen(PORT, () => {
--- frontend/src/lib/axios.js
+++ frontend/src/lib/axios.js
@@ -1,7 +1,9 @@
 import axios from "axios"
 
-const axiosInstance = axios.create({
-    baseURL : "http://localhost:5001/api"
-})
+const BASE_URL = import.meta.env.MODE === "development" ? "http://localhost:5001/api" : "/api"
+
+const api = axios.create({
+    baseURL: BASE_URL,
+});
 
 export default axiosInstance
